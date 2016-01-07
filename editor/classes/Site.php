b77a9918c70970f915a9220de6c16a7058da0aaa
Yannick Felix <yannick.felix1999@gmail.com>
2016-01-07 16:59:45 +0100

    ICMS - right_tag to actionkey - PHPInfo updates

---
+++ editor/classes/Site.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yanni
+ * Date: 07.01.2016
+ * Time: 16:46
+ */
+
+namespace ICMS;
+
+
+class Site {
+    private $vID, $pID, $name, $type, $authorID, $lastAuthorID, $lastEditDate, $version, $state;
+
+
+}
